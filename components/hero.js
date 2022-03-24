58ff35b477dd153d06815af0afb2e50d450ca6a0
blaze vincent <hikikomareep@pop-os.localdomain>
2022-03-24 01:50:27 -0500

    Add placeholder breadcrumb trail

--- components/hero.js
+++ components/hero.js
@@ -2,6 +2,11 @@ import Carousel from "./generic/carousel/carousel";
 
 export default function Hero(){
   return <div className="w-full">
+    <div 
+      className="w-full max-w-screen-lg bg-blue-500 m-auto p-4"
+    >
+      <p>Home</p>
+    </div>
     <Carousel />
   </div>
 }
