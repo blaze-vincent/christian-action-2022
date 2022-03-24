8bc989608e572715451838a0595c9c778c6f394d
blaze vincent <hikikomareep@pop-os.localdomain>
2022-03-24 01:47:56 -0500

    Reform navbar, add nav items

--- components/header.js
+++ components/header.js
@@ -1,7 +1,18 @@
+import Link from "next/link";
+import Navbar from "./generic/navbar/navbar";
+
 export default function Header(){
-  return (<header>
-    <h1 className="text-black text-2xl font-ibm-sans">
-      Christian Action, Inc. 
-    </h1>
+  return (<header
+    className="flex px-4 sm:flex-row items-center flex-col sm:gap-4 justify-between w-full max-w-screen-lg m-auto"
+  >
+    <Link href='/'>
+      <a>
+        <h1 className="text-neutral-800 text-2xl whitespace-nowrap md:text-4xl font-medium font-ibm-serif py-4">
+          Christian Action, Inc. 
+        </h1>
+      </a>
+    </Link>
+
+    <Navbar />
   </header>)
 }
