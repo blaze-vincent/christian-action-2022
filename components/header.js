f91f31728a67008b98c3e911217541535457fd6e
blaze vincent <hikikomareep@pop-os.localdomain>
2022-03-25 15:32:59 -0500

    Move placeholder breadcrumb trail to header

--- components/header.js
+++ components/header.js
@@ -3,16 +3,31 @@ import Navbar from "./generic/navbar/navbar";
 
 export default function Header(){
   return (<header
-    className="flex px-4 py-1 sm:flex-row items-center flex-col sm:gap-4 justify-between w-full max-w-screen-lg m-auto"
+    className="w-full max-w-screen-lg m-auto flex flex-col gap-1 pb-2 text-neutral-800 "
   >
-    <Link href='/'>
-      <a>
-        <h1 className="text-neutral-800 text-2xl whitespace-nowrap md:text-4xl font-medium font-ibm-serif py-4">
-          Christian Action, Inc. 
-        </h1>
-      </a>
-    </Link>
 
-    <Navbar />
+    {/* 'logo' and navbar*/}
+    <div className="flex p-4 sm:flex-row items-center flex-col sm:gap-4 justify-between w-full">
+      <Link href='/'>
+        <a>
+          <h1 className="text-2xl relative hover:underline whitespace-nowrap md:text-4xl font-medium font-ibm-serif">
+            Christian Action, Inc. 
+          </h1>
+        </a>
+      </Link>
+
+      <Navbar />
+    </div>
+
+    {/* placeholder breadcrumb trail */}
+    <div 
+      className="
+      w-full max-w-screen-lg m-auto p-4
+      bg-blue-400 text-neutral-800
+      border-t-2 border-blue-500 
+      "
+    >
+      <p>Home</p>
+    </div>
   </header>)
 }
--- components/hero.js
+++ components/hero.js
@@ -2,11 +2,6 @@ import Carousel from "./generic/carousel/carousel";
 
 export default function Hero(){
   return <div className="w-full">
-    <div 
-      className="w-full max-w-screen-lg bg-blue-500 m-auto p-4"
-    >
-      <p>Home</p>
-    </div>
     <Carousel />
   </div>
 }
