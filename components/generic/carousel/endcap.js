b631b1ce10f24d44ddf1581a967e65b0f6c7e28d
Blaze Vincent <Blaze.Vincent@DRAKEHS.LOCAL>
2022-03-28 09:06:10 -0500

    Separate arrow svg into own component

--- components/generic/carousel/endcap.js
+++ components/generic/carousel/endcap.js
@@ -1,4 +1,5 @@
 import { useEffect, useRef } from "react"
+import Arrow from "../svg/arrow"
 
 
 export default function Endcap({side = 'left', slidesCount}){
@@ -17,15 +18,8 @@ export default function Endcap({side = 'left', slidesCount}){
   }, [] /*side doesn't ever change so no need for dependency*/)
 
   //material arrow icon
-  return <svg 
+  return <Arrow
     ref={element}
-    className={"fill-stone-200 absolute " + sideClasses[side]}
-    xmlns="http://www.w3.org/2000/svg" 
-    height="24" 
-    viewBox="0 0 24 24" 
-    width="24"
-    >
-      <path d="M0 0h24v24H0z" fill="none"/>
-      <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/>
-    </svg>
+    className={sideClasses[side]}
+  />
 }
---
+++ components/generic/svg/arrow.js
@@ -0,0 +1,21 @@
+import { forwardRef } from "react"
+
+const Arrow = forwardRef(({className, alt, title}, ref) => {
+  return <svg 
+    alt={alt}
+    ref={ref}
+    className={"fill-stone-200 absolute " + className}
+    
+    xmlns="http://www.w3.org/2000/svg" 
+    height="24" 
+    viewBox="0 0 24 24" 
+    width="24"
+    >
+      {title && <title>{title}</title>}
+      
+      <path d="M0 0h24v24H0z" fill="none"/>
+      <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/>
+  </svg>
+})
+
+export default Arrow;
