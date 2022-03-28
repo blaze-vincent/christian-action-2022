9871e54c36462f6d46d3dd687d66322175587d7c
Blaze Vincent <Blaze.Vincent@DRAKEHS.LOCAL>
2022-03-28 09:11:29 -0500

    Remove hardcoded styling from carousel

--- components/hero.js
+++ components/hero.js
@@ -1,7 +1,7 @@
 import Carousel from "./generic/carousel/carousel";
 
 export default function Hero(){
-  return <div className="w-full">
+  return <div className="w-full max-w-screen-lg m-auto">
     <Carousel />
   </div>
 }
