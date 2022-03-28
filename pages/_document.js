088267f5e0dfa158535937c09d684d9b1f3b7949
Blaze Vincent <Blaze.Vincent@DRAKEHS.LOCAL>
2022-03-28 14:05:59 -0500

    Specify weights for IBM Plex Sans

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
-          <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans&family=IBM+Plex+Serif:wght@500;600&display=swap" rel="stylesheet" />  
+          <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400;500&family=IBM+Plex+Serif:wght@500;600&display=swap" rel="stylesheet" />  
         </Head>
         <body>
           <Main />
