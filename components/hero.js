ed759a3f4c79a79f0864637260b77117a40f848f
Blaze Vincent <Blaze.Vincent@DRAKEHS.LOCAL>
2022-03-28 09:40:03 -0500

    Add placeholder text section to hero

--- components/hero.js
+++ components/hero.js
@@ -1,7 +1,19 @@
 import Carousel from "./generic/carousel/carousel";
 
 export default function Hero(){
-  return <div className="w-full max-w-screen-lg m-auto">
-    <Carousel />
+  return <div className="w-full max-w-screen-lg m-auto grid sm:grid-cols-3 gap-2">
+    <Carousel 
+      className='sm:col-span-2'
+    />
+    <div
+      className="bg-blue-400 p-4 flex flex-col gap-4"
+    >
+      <h2 className="font-ibm-serif font-medium text-xl">
+        Lorem Ipsum
+      </h2>
+      <p>
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+      </p>
+    </div>
   </div>
 }
