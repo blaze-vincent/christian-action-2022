90b3f2e00c3e27611fcd1b5ab70e1225363a7216
Blaze Vincent <Blaze.Vincent@DRAKEHS.LOCAL>
2022-03-28 15:05:41 -0500

    Add placeholder 'back to top' button to footer

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,20 @@
+import Arrow from "./generic/svg/arrow";
+
 export default function Footer(){
-  return (<footer className="bg-stone-800 text-white p-4">
-    footer tag contents
+  return (<footer className="bg-stone-800 text-white p-4 mt-4">
+    <div
+      className="w-full max-w-screen-lg m-auto"
+    >
+      back to top
+      <button
+        
+      >
+        <Arrow 
+          className='rotate-90 relative w-8 h-8' 
+          alt='back to top'
+          title='back to top'  
+        />
+      </button>
+    </div>
   </footer>)
 }
