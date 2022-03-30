f5862efebd3a09e9baf7b70b9d637083576bc86d
Blaze Vincent <Blaze.Vincent@DRAKEHS.LOCAL>
2022-03-30 09:02:08 -0500

    Add 'back to top' button functionality

--- components/footer.js
+++ components/footer.js
@@ -1,14 +1,25 @@
 import Arrow from "./generic/svg/arrow";
+import { useEffect, useRef } from 'react';
 
 export default function Footer(){
+
+  const scrollToTop = _ => {
+     window.scrollTo({
+       top: 0,
+       behavior: 'smooth'
+     })
+  }
+
   return (<footer className="bg-stone-800 text-white p-4 mt-4">
+    {/* width container */}
     <div
       className="w-full max-w-screen-lg m-auto"
     >
-      back to top
       <button
-        
+        className="flex flex-row gap-2 items-center"
+        onClick={scrollToTop}
       >
+        back to top
         <Arrow 
           className='rotate-90 relative w-8 h-8' 
           alt='back to top'
