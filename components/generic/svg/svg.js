bd05c40dfa145ca0e437902c97c09681c6b6abd0
Blaze Vincent <Blaze.Vincent@DRAKEHS.LOCAL>
2022-04-27 09:20:52 -0500

    Enforce svg width

--- components/generic/svg/svg.js
+++ components/generic/svg/svg.js
@@ -1,9 +1,9 @@
 //generic svg template for including static svg files
-//created because certain SVGs had strange document-breaking behavior when not in separate files
+//created because certain SVGs had strange document-breaking behavior when not encapsulated
 
 export default function SVG({src, className}){
   return <div 
-    className={"relative w-24 h-24 overflow-hidden flex items-center justify-center" + className}
+    className={"relative w-24 h-24 min-w-[6rem] min-h-[6rem] overflow-hidden flex items-center justify-center " + className}
     >
       <img src={src} alt='' className="absolute w-full h-full left-0 top-0 p-1"/>
   </div>
