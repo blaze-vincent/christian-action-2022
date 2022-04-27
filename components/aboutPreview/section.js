2725c4a1de8e6136d865c93681313cc85cf51021
Blaze Vincent <Blaze.Vincent@DRAKEHS.LOCAL>
2022-04-27 09:19:28 -0500

    Add padding for mobile curved-screen readability

--- components/aboutPreview/section.js
+++ components/aboutPreview/section.js
@@ -3,9 +3,9 @@ import SVG from "../generic/svg/svg";
 export default function Section({imgSrc, textContent, }){
 
   return <div
-    className="flex flex-row gap-4 w-full items-end"
+    className="flex flex-row gap-4 w-full items-end pr-4"
   >
-    <SVG src={imgSrc} className='bg-blue-300' />
+    <SVG src={imgSrc} />
     <p
       className="text-lg max-w-xl"
     >{textContent}</p>
