76990f9e84c47ddf2788dbd59125dec0f9972c8b
Blaze Vincent <Blaze.Vincent@DRAKEHS.LOCAL>
2022-04-27 09:16:15 -0500

    Increase about banner width

--- components/aboutPreview/aboutPreview.js
+++ components/aboutPreview/aboutPreview.js
@@ -6,7 +6,7 @@ export default function AboutPreview(){
     className="w-full max-w-screen-lg flex flex-col gap-4"
   >
     <h2
-      className="text-2xl font-ibm-serif font-medium max-w-max bg-blue-300 border-solid border-t-2 border-blue-500 p-2 "
+      className="text-2xl font-ibm-serif font-medium max-w-max bg-blue-300 px-8 border-solid border-t-2 border-blue-500 p-2 "
     >About us</h2>
     <Section imgSrc={'/svg/house-broken.svg'} textContent='Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'/>
     <Section imgSrc={'/svg/help.svg'} textContent='Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'/>
