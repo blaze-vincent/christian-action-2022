3032d54faed504480392f9414f13f1f1134cec8a
Blaze Vincent <Blaze.Vincent@DRAKEHS.LOCAL>
2022-05-02 13:22:53 -0500

    Move Give page contents to homepage section

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,18 @@ export default function Home() {
   return (<>
     <Hero />
     <AboutPreview />
+
+    {/* donation information, not enough to warrant its own page */}
+    <div
+      className="bg-blue-200 p-4 flex flex-col gap-4 w-full max-w-screen-lg"
+    >
+      <h2
+        id='give'
+        className="text-2xl font-medium font-ibm-serif"
+      >Christian Action is primarily funded through donations.</h2>
+      <p>If you are considering donation, please reach out to Christian Action, Inc. through one of the contacts listed <a className="underline" href="/resources">here</a>.</p>
+      <p>Your contribution may be tax-deductible within the limits prescribed by the law.</p>
+    </div>
   </>
   )
 }
