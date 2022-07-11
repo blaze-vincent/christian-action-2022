d14a0b8b6a24fb8b14a63aebed32dba7c18d3009
blaze@drakehs <blaze.vincent@drakehs.com>
2022-07-11 08:44:26 -0500

    Remove background color; Increase font weight

--- components/sectionTitle.js
+++ components/sectionTitle.js
@@ -1,5 +1,5 @@
-export default function SectionTitle({title}){
+export default function SectionTitle({title, children}){
   return <h2
-    className="text-2xl font-baloo font-medium max-w-max bg-blue-200 px-8 border-solid border-t-2 border-blue-500 p-2 "
-  >{title}</h2>
+    className="text-2xl font-baloo font-bold max-w-max px-8 border-solid border-t-2 border-blue-500 p-2 "
+  >{children || title}</h2>
 }
