80a653e353deb8b0e105bad7cec74bda702351ce
blaze@drakehs <blaze.vincent@drakehs.com>
2022-07-12 14:32:30 -0500

    Remove acknowledgment of demographic circumstances; Change link style

--- components/aboutPreview/aboutPreview.js
+++ components/aboutPreview/aboutPreview.js
@@ -8,14 +8,14 @@ export default function AboutPreview(){
   >
     <SectionTitle title="About us" />
 
-    <Section imgSrc={'/svg/house-broken.svg'} textContent='Our services are intended for those in difficult situations in need of housing and loan support.'/>
+    <Section imgSrc={'/svg/house-broken.svg'} textContent='Our services are intended for those in need of housing and loan support.'/>
     <Section imgSrc={'/svg/help.svg'} textContent='We aid people in achieving a comfortable living situation. The services we offer include partial down payments and zero-interest loans.'/>
     <Section imgSrc={'/svg/house.svg'} textContent='With our help, you can get back on your feet and live comfortably.'/>
     <Link 
       href='/about'       
     >
       <a
-        className="font-medium underline px-2 py-1 max-w-max"
+        className="px-3 py-2 max-w-max bg-blue-300 hover:bg-blue-400 font-medium"
       >Read more about us</a>
     </Link>
   </div>
