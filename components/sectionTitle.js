758522bb5d57fbbd043924a7ddd3de8459a3a71f
blaze@drakehs <blaze.vincent@drakehs.com>
2022-07-13 15:21:19 -0500

    Add subsection styling

--- components/sectionTitle.js
+++ components/sectionTitle.js
@@ -1,5 +1,15 @@
-export default function SectionTitle({title, children}){
+export default function SectionTitle({title, children, subsection = false, id}){
+  const className = `
+    font-baloo max-w-max border-solid border-t-2 
+    ${
+      subsection 
+      ? "text-xl font-medium p-1 px-4 border-blue-300" 
+      : "text-2xl font-bold p-2 px-8 border-blue-500"
+    }
+  `;
+
   return <h2
-    className="text-2xl font-baloo font-bold max-w-max px-8 border-solid border-t-2 border-blue-500 p-2 "
+    id={id}
+    className={className}
   >{children || title}</h2>
 }
