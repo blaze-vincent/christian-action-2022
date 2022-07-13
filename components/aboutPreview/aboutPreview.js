9cb21fbd429be4ac96c054a370263460213b1e2b
blaze@drakehs <blaze.vincent@drakehs.com>
2022-07-13 15:20:23 -0500

    Add mission statement

--- components/aboutPreview/aboutPreview.js
+++ components/aboutPreview/aboutPreview.js
@@ -6,10 +6,12 @@ export default function AboutPreview(){
   return <div
     className="w-full max-w-screen-lg flex flex-col gap-4"
   >
-    <div className="pl-4">
+    <div className="pl-4 ">
       <SectionTitle title="About us" />
+      <p className="text-lg relative font-medium">
+        Christian Action is a Burlington nonprofit whose mission is to help Burlington families gain homeownership. Since 1968, Christian Action has partnered with state and local agencies to support families in making down payments, managing purchase contracts, and maintaining affordable homes.
+      </p>
     </div>
-
     <Section imgSrc={'/svg/house-broken.svg'} textContent='Our services are intended for those in need of housing and loan support.'/>
     <Section imgSrc={'/svg/help.svg'} textContent='We aid people in achieving a comfortable living situation. The services we offer include partial down payments and zero-interest loans.'/>
     <Section imgSrc={'/svg/house.svg'} textContent='With our help, you can get back on your feet and live comfortably.'/>
