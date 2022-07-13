5b61c2fe1e60aa7757646564c763a7d34c974f96
blaze@drakehs <blaze.vincent@drakehs.com>
2022-07-13 08:30:18 -0500

    Alphabetize contacts

--- components/contactInformation/contactInformation.js
+++ components/contactInformation/contactInformation.js
@@ -3,40 +3,40 @@ import Contact from "./contact"
 
 const contacts = [
   {
-    name: 'Roger Hatteberg',
-    email: 'rogerhatteberg@msn.com'
-  },
-  {
     name: 'Brice Hughes',
     email: 'pastor@zionuccburlington.com'
-  },
-  {
-    name: 'Tom Richert',
-    email: 'tomtomrichett@gmail.com'
-  },
-  {
-    name: 'John Riessen',
-    email: 'jhriessen@gmail.com'
-  },
-  {
-    name: 'Robert Ritson',
-    email: 'robatbbt@hotmail.com'
   },
   {
     name: 'Reverend David Selmon',
     email: 'dselmon431@gmail.com'
   },
   {
-    name: 'Reverend Susanne Smith',
-    email: 'revses@aol.com'
+    name: 'John Riessen',
+    email: 'jhriessen@gmail.com'
+  },
+  {
+    name: 'Marshal Walz',
+    email: 'marshal_walz@msn.com'
   },
   {
     name: 'Reverend Orlando "Ray" Dial',
     email: 'odial5542@msn.com'
   },
   {
-    name: 'Marshal Walz',
-    email: 'marshal_walz@msn.com'
+    name: 'Robert Ritson',
+    email: 'robatbbt@hotmail.com'
+  },
+  {
+    name: 'Roger Hatteberg',
+    email: 'rogerhatteberg@msn.com'
+  },
+  {
+    name: 'Reverend Susanne Smith',
+    email: 'revses@aol.com'
+  },  
+  {
+    name: 'Tom Richert',
+    email: 'tomtomrichett@gmail.com'
   },
 ]
 
