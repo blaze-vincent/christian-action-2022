65916e1b71f274b9de626a697cb5e900c808fb7b
blaze@drakehs <blaze.vincent@drakehs.com>
2022-07-18 10:26:09 -0500

    Standardize call-to-action button; Remove 'back on your feet'

--- components/aboutPreview/aboutPreview.js
+++ components/aboutPreview/aboutPreview.js
@@ -1,6 +1,6 @@
 import Section from "./section"
-import Link from 'next/link'
 import SectionTitle from "../sectionTitle"
+import CallToAction from "../callToAction"
 
 export default function AboutPreview(){
   return <div
@@ -12,17 +12,13 @@ export default function AboutPreview(){
         Christian Action is a Burlington nonprofit whose mission is to help Burlington families gain homeownership. Since 1968, Christian Action has partnered with state and local agencies to support families in making down payments, managing purchase contracts, and maintaining affordable homes.
       </p>
     </div>
-    <Section imgSrc={'/svg/house-broken.svg'} textContent='Our services are intended for those in need of housing and loan support.'/>
+    <Section imgSrc={'/svg/people.svg'} textContent='Our services are intended for those in need of housing and loan support in Burlington.'/>
     <Section imgSrc={'/svg/help.svg'} textContent='We aid people in achieving a comfortable living situation. The services we offer include partial down payments and zero-interest loans.'/>
-    <Section imgSrc={'/svg/house.svg'} textContent='With our help, you can get back on your feet and live comfortably.'/>
+    <Section imgSrc={'/svg/house.svg'} textContent='Christian Action, Inc. has helped hundreds of Burlington families acquire housing.'/>
     <div className="pl-4">
-      <Link 
-        href='/about'
-      >
-        <a
-          className="px-3 py-2 max-w-max bg-blue-300 hover:bg-blue-400 font-medium"
-        >Read more about us</a>
-      </Link>
+      <CallToAction href='/about'>
+        Read more about us
+      </CallToAction>
     </div>
     
   </div>
