8d8cb11201e890b7aab018baaa78bd56c418f5e3
blaze@drakehs <blaze.vincent@drakehs.com>
2022-07-18 10:32:03 -0500

    Increase gap

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
         Home - Christian Action, Inc.
       </title>
     </Head>
-    <div className="flex flex-col gap-16">
+    <div className="flex flex-col gap-20">
       <Hero />
       <AboutPreview />
       <DonationSection />
